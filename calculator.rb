3d1043398fddc762c6de99072449d4c6191d009d
Zachary Mayle <zam1998@gmail.com>
2019-11-12 20:14:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,8 @@ first_number = 8
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
-quotient = first_number 
+product = first_number * second_number
+quotient = first_number / second_number
+
+
+puts quotient
